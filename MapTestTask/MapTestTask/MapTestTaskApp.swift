c263fe238ba445116d43bfe98fdc1a462458e997
Artem Vishniakov <artem.vishniakov95@yandex.ru>
2023-10-26 20:13:47 +0400

    Initial commit

---
+++ MapTestTask/MapTestTask/MapTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestTaskApp.swift
+//  MapTestTask
+//
+//  Created by Artem Vishniakov on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
